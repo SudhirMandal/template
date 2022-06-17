7f1bcf472d14dc1f80d0b1ab9c1ebce67c245617
Sudhir <r.sudhirmandal@gmail.com>
2022-06-17 16:01:46 +0530

    Diff between applyThen and compose method of Function

--- demo/src/main/java/com/example/demo/java8/FunctionChainingExamle.java
+++ demo/src/main/java/com/example/demo/java8/FunctionChainingExamle.java
@@ -15,7 +15,11 @@ public class FunctionChainingExamle {
 		System.out.println(f2.apply(nickName));
 		System.out.println(f1.andThen(f2).apply(name));
 		System.out.println(f1.compose(f2).apply(nickName));
-		
+
+		Function<Integer, Integer> f3 = i -> i + i;
+		Function<Integer, Integer> f4 = i -> i * i * i;
+		System.out.println(f3.andThen(f4).apply(2));
+		System.out.println(f3.compose(f4).apply(2));
 
 	}
 
