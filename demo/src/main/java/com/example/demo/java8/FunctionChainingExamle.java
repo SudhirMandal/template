3c0eb5b1944b094390044175e2e9210579798afb
Sudhir <r.sudhirmandal@gmail.com>
2022-06-17 15:54:57 +0530

    FunctionChaining

---
+++ demo/src/main/java/com/example/demo/java8/FunctionChainingExamle.java
@@ -0,0 +1,22 @@
+package com.example.demo.java8;
+
+import java.util.function.Function;
+
+public class FunctionChainingExamle {
+
+	public static void main(String[] args) {
+		String name = "AishwariyaAbhi";
+		String nickName = "Aish";
+		Function<String, String> f1 = (String s1) -> s1.substring(0, 4);
+		Function<String, String> f2 = (String s1) -> s1.toUpperCase();
+		System.out.println(f1.apply(name));
+		System.out.println(f2.apply(name));
+		System.out.println(f1.apply(nickName));
+		System.out.println(f2.apply(nickName));
+		System.out.println(f1.andThen(f2).apply(name));
+		System.out.println(f1.compose(f2).apply(nickName));
+		
+
+	}
+
+}
